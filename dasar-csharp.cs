bb3bb67d3f83f155efce6ade568081ff80feba46
Fahmi Anwar <fahmi@saungawi.com>
2021-04-28 14:20:57 +0700

    Create dasar-csharp.cs

---
+++ dasar-csharp.cs
@@ -0,0 +1,32 @@
+//Variable
+        //variablenya kataPerkenalan = value
+        //untuk menghitung -> int, float, double
+        //untuk karakter -> char, string
+        // untuk biner (true/false) -> bool 
+        
+        //Operators
+        //aritmatika ++ --
+        
+        //comparison
+        
+        // logical
+        
+        //    P        Q        ||            &&
+        //    T        T        T            T
+        //    T        F        T            F
+        //    F        T        T            F
+        //    F        F        F            F
+        
+        //assignment
+        
+        //If Statement
+        
+        //if (bool){}
+        //else if(){}
+        //else{}
+
+        //Array / Larik
+        
+        //For -> for(expression 1; expression 2; expression 3) foreach(var in array)
+        
+        //Method -> Void / Non-Void
